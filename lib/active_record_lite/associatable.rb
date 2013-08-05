6f719b890d4d9998354b5271d5fb0523884f1df3
William Li and Rashi Abramson <student@appacademy.io>
2013-08-05 14:59:52 -0700

    Added more stuff.

--- lib/active_record_lite/associatable.rb
+++ lib/active_record_lite/associatable.rb
@@ -4,14 +4,22 @@ require_relative './db_connection.rb'
 
 class AssocParams
   def other_class
+    @other_class_name.constantize
   end
 
   def other_table
+    p self.other_class
+    self.other_class.table_name
   end
 end
 
 class BelongsToAssocParams < AssocParams
+  attr_reader :primary_key, :foreign_key
+  attr_reader :other_table_name
   def initialize(name, params)
+    @other_class_name = params[:class_name] || name.to_s.camelize
+    @primary_key = params[:primary_key] || :id
+    @foreign_key = params[:foreign_key] || "#{name}_id".to_sym
   end
 
   def type
@@ -19,7 +27,13 @@ class BelongsToAssocParams < AssocParams
 end
 
 class HasManyAssocParams < AssocParams
-  def initialize(name, params, self_class)
+  attr_reader :other_class_name
+  attr_reader :other_table_name, :primary_key, :foreign_key
+  def initialize(name, params)
+    @other_class_name = params[:class_name] || name.to_s.singularize.camelize
+    @primary_key = params[:primary_key] || :id
+    @foreign_key = params[:foreign_key] ||
+    "#{self.class.to_s.underscore_id}".to_sym
   end
 
   def type
@@ -28,34 +42,44 @@ end
 
 module Associatable
   def assoc_params
+    @assoc_params || {}
   end
 
   def belongs_to(name, params = {})
     # self -> class
+    assoc_params[name] = BelongsToAssocParams.new(name,params)
+    aparams = BelongsToAssocParams.new(name, params)
     self.send(:define_method, name) do
       # self -> instance
-      other_class_name = params[:class_name] || name.camelize
-      primary_key = params[:primary_key] || :id
-      foreign_key = params[:foreign_key] || "#{name}_id".to_sym
-      other_class = other_class_name.constantize
-      other_table_name = other_class.table_name
 
-      row = DBConnection.execute(<<-SQL)
-      SELECT "#{other_table_name}".* FROM #{other_table_name}
-      INNER JOIN #{self.class.table_name}
-      ON #{self.class.table_name}.#{foreign_key}
-      = #{other_table_name}.#{primary_key};
+
+      my_sql = <<-SQL
+      SELECT "#{aparams.other_table}".* FROM #{aparams.other_table}
+      INNER JOIN "#{self.class.table_name}"
+      ON "#{self.class.table_name}".#{aparams.foreign_key}
+      = "#{aparams.other_table}".#{aparams.primary_key};
       SQL
-      other_class.parse_all(row)
+      row = DBConnection.execute(my_sql)
+      aparams.other_class.parse_all(row)
     end
   end
 
   def has_many(name, params = {})
+    aparams = HasManyAssocParams.new(name, params)
     self.send(:define_method, name) do
 
+
+      row = DBConnection.execute(<<-SQL)
+      SELECT "#{aparams.other_table}".* FROM #{aparams.other_table}
+      INNER JOIN #{self.class.table_name}
+      ON #{aparams.other_table}.#{aparams.foreign_key}
+      = #{self.class.table_name}.#{aparams.primary_key};
+      SQL
+      aparams.other_class.parse_all(row)
     end
   end
 
   def has_one_through(name, assoc1, assoc2)
+
   end
 end
